7edf23a685c2d46d14279e7198f9755ebfc5f06e
Thom Wiggers <thom@thomwiggers.nl>
2019-09-18 14:38:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Extract biblatex entries from big bibliographies
+
+This is a python script that will allow you to extract entries from large bibliographies.
+
+For example, if you're using [cryptobib][cryptobib], it may take a few minutes to compile everything.
+
+Not anymore! This script will take your biblatex `bcf` file and then extract the needed entries from larger bibfiles.
+
+
+## Makefile example
+
+```Makefile
+.PHONY: main.pdf
+main.pdf: extracted_cryptobib.bib
+	./latexrun --bibtex-cmd=biber -Wall main.tex
+
+latex.out/main.bcf:
+	mkdir -p latex.out
+	pdflatex -interaction=batchmode -output-directory=latex.out main
+
+extracted_cryptobib.bib: latex.out/main.bcf cryptobib/crypto.bib
+	python3 extract_from_bibliography.py $^ > $@
+```
+
---
+++ extract_from_bibliography.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import re
+import logging
+import xml.etree.ElementTree as ET
+
+
+NAMESPACES = {'bcf': "https://sourceforge.net/projects/biblatex"}
+
+
+def get_keys(filename):
+    parser = ET.parse(filename)
+    root = parser.getroot()
+    return {item.text for item in root.findall('.//bcf:citekey', NAMESPACES)}
+
+
+def get_items_from_bib(keys, filename):
+    with open(filename, 'r') as f:
+        contents = f.read()
+    regex = re.compile(
+        r"""
+        ^@ \w+ \{ (%s),$\n          # KEY
+          .*?                    # BODY
+        ^\}$""" % '|'.join(keys),
+        re.MULTILINE | re.VERBOSE | re.DOTALL)
+    for match in regex.finditer(contents):
+        string = match.group(0)
+        logging.info("Found %s", string.split('\n')[0])
+        yield string
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} <bcf-file> <bibfile...>")
+        sys.exit(1)
+    bcf_file = sys.argv[1]
+    bibfiles = sys.argv[2:]
+    keys = get_keys(bcf_file)
+    for bibfile in bibfiles:
+        for item in get_items_from_bib(keys, 'cryptobib/crypto.bib'):
+            print(item)
+            print()
